0300e3eccd8325c4fbb12ad346980e590ff19aa1
Rajitha-Madushan-Fernando <buddikarajitha@yahoo.com>
2022-01-26 00:57:19 +0100

    Setup urls, route and template for django

--- src/django_crud/urls.py
+++ src/django_crud/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from pages.views import contact_view, home_view, about_view
+
+
 urlpatterns = [
+    path('',home_view, name='home'),
+    path('contact/', contact_view),
+    path('about/', about_view),
     path('admin/', admin.site.urls),
 ]
